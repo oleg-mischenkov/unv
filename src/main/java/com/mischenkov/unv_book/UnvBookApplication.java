cf46a3bd6ca135f63cdc8e14630f6899059ec5e1
Oleg Mischenkov <mischenkov.oleg@gmail.com>
2024-12-16 20:29:12 +0100

    the entry configs

---
+++ src/main/java/com/mischenkov/unv_book/UnvBookApplication.java
@@ -0,0 +1,13 @@
+package com.mischenkov.unv_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnvBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnvBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mischenkov/unv_book/UnvBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mischenkov.unv_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnvBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
